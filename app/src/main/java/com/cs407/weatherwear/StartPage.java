8f722308869c615d7d04201a99a29db9e71ade6f
ddelan <dtnguyen0216@gmail.com>
2023-11-03 15:29:18 -0500

    Added Logo to first page and started SecondPage

--- app/src/main/java/com/cs407/weatherwear/StartPage.java
+++ app/src/main/java/com/cs407/weatherwear/StartPage.java
@@ -1,8 +1,10 @@
 package com.cs407.weatherwear;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstpage);
+
+        Button goButton = findViewById(R.id.goButton);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // When the button is clicked, start the SecondActivity
+                Intent intent = new Intent(StartPage.this, SecondPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
